ef168c0abe4965acf606ee792be72440b5ef8a7e
danast2 <danast_2@mail.ru>
2024-01-19 18:00:06 +0300

    19.01.24/18:00

--- classes/ex5.cpp
+++ classes/ex5.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 class Date {
 public:
-  enum Month { jan = 1, feb, mar, apr, may, jun, ju, aug, sep, oct, nov, dec };
+  enum Month { jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec };
   class Bad_date {}; // класс исключений
   Date(int dd = 0, Month mm = Month(0), int yy = 0);
 
@@ -22,5 +24,80 @@ private:
   int d, m, y;
   static Date default_date;
 };
+
+Date Date::default_date(22, jan, 1901);
+
+// просто пример реализации
+bool midnigth() {
+  // сюда передается время, если 00:00 => true, иначе => false
+  return true;
+}
+
+void func(Date &d) {
+  Date lvb_day = Date(16, Date::dec, d.year());
+  if (d.day() == 29 && d.month() == Date::feb) {
+    //.......
+  }
+  if (midnigth())
+    d.add_day(1);
+  std::cout << "day after: " << d.day() << '\n';
+}
+
+int leapyaer(int &yy) {
+  // тут функция проверяет год, если високосный, то => return 1, если нет, то
+  // return 0
+  return 1;
+}
+
+Date::Date(int dd, Month mm, int yy) {
+  if (yy == 0)
+    yy = default_date.year();
+  if (mm == 0) {
+    mm = default_date.month();
+  }
+  if (dd == 0) {
+    dd = default_date.day();
+  }
+  int max;
+  switch (mm) {
+  case feb:
+    max = 28 + leapyaer(yy);
+    break;
+  case apr:
+  case jun:
+  case sep:
+  case nov:
+    max = 30;
+    break;
+  case jan:
+  case mar:
+  case may:
+  case jul:
+  case aug:
+  case oct:
+  case dec:
+    max = 31;
+    break;
+  default:
+    throw Bad_date(); // выдаем ошибку
+  }
+  if (dd < 1 || max < dd)
+    throw Bad_date();
+  y = yy;
+  m = mm;
+  d = dd;
+}
+
+/*void fill(std::vector<Date>& a){
+  while (std::cin) {
+    Date d;
+    try {
+      std::cin >> d;
+    } catch (Date::Bad_date) { // обработка ошибки
+      continue;
+    }
+    aa.
+  }
+}*/
 
 int main() { return 0; }
