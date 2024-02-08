320827c82e43282408409c0991c38ee3c9c8eb0e
Daniil <danast_2@mail.ru>
2024-02-08 07:23:59 +0300

    08.02.2024/07:23

--- templates/tem_ex2.cpp
+++ templates/tem_ex2.cpp
@@ -19,3 +19,25 @@ Buffer<char, 128> cbuf;
 Buffer<int, 5000> ibuf;
 //Buffer<Record, 8> rbuf;
 
+//эквивалентность типов
+
+String<char> s1;
+String<unsigned char> s2;
+String<int> s3;
+
+typedef unsigned char Uchar;
+String<Uchar> s4;
+String<char> s5;
+
+Buffer<String<char>, 10> b1;
+Buffer<char, 10> b2;
+Buffer<char, 20 - 10> b3;
+
+/*
+    Предоставляя один и тот же набор аргументов шаблона, мы имеем дело с одним
+и тем же сгенерированным типом. Но что в данном контексте означает «один и тот
+же»? Так как typedef не создает нового типа, то String<Uchar> есть то же самое, что
+и String<unsigned char>. И наоборот, так как char и unsigned char суть разные типы
+(§4.3), то и типы String<char> и String<unsigned char> тоже разные.
+Компилятор может вычислять константные выражения, поэтому тип Buffer<char, 20-10> эквивалентен типу Buffer<char, 10>.
+ */
