90fcf8af9877bf2d5b04d925ed8ade4fa01add3f
Daniil <danast_2@mail.ru>
2024-02-09 20:51:42 +0300

    09.02.2024/20:51

--- templates/tem_ex9.cpp
+++ templates/tem_ex9.cpp
@@ -14,6 +14,50 @@ template<class T> void sort(Vector<T>& v){
             break;
 }
 
-template<> bool less<const char*>(const char* a, const char* b){
+/*template<> bool less<const char*>(const char* a, const char* b){
     return strcmp(a, b) < 0;
+}*/
+
+/*Как и для классовых шаблонов , для функциональных шаблонов префикс
+template означает специализацию, не нуждающуюся в параметрах шаблона.
+
+Конструкция <const char*> после имени функции означает, что эта специализация
+должна использоваться в случае, когда фактические параметры вызова имеют тип
+const char*. Поскольку параметры функциональных шаблонов могут быть
+выведены из фактических параметров вызова, нет нужды специфицировать их явно.
+Поэтому мы можем упростить определение специализации:
+*/
+
+/*template<> bool less<>(const char* a, const char* b)
+{
+return strcmp (a, b) < 0;
+}*/
+
+
+template<> bool less(const char* a, const char* b)
+{
+return strcmp (a, b) < 0;
+}
+
+//рассмотрим очевидное определение для swap()
+
+//Такое решение неэффективно для типа Vector, поскольку при этом будут
+//копироваться все элементы векторов. Элементы х будут даже копироваться дважды.
+template<class T> void swap(T& x, T& y){
+    T t;
+    x = y;
+    y = t;
+}
+
+template<class T> void Vector<T>::swap(Vector& a) //меняемся представлениями
+{
+    swap (v, a.v) ;
+    swap (sz, a.sz) ;
+}
+
+//Теперь эту функцию-член можно использовать в качестве альтернативы общему
+// шаблону swap ():
+
+template<class T> void swap(Vector<T>& a, Vector<T>& b){
+    a.swap(b);
 }
