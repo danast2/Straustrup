a822f171ff7b53dde9a66d854ee362adc20e0b73
Daniil <danast_2@mail.ru>
2024-02-10 07:29:20 +0300

    10.02.2024/07:29

--- templates/tem_ex11.cpp
+++ templates/tem_ex11.cpp
@@ -1,3 +1,15 @@
-//
-// Created by Даниил Асташов on 10.02.2024.
-//
+//класс или шаблонный класс могут иметь члены, которые сами являются шаблонами.
+
+template<class Scalar> class complex{
+    Scalar re, im;
+public:
+    template<class T> complex(const complex<T>& c): re(c.real()), im(c.imag()){}
+
+    Scalar real() const{
+        return re;
+    }
+    Scalar image() const{
+        return im;
+    }
+    //.......
+};
