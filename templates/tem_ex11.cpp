e3f2ab616c8eee2dc8b64db5677980c834f74fe6
Daniil <danast_2@mail.ru>
2024-02-10 09:57:31 +0300

    10.02.2024/09:57

--- templates/tem_ex11.cpp
+++ templates/tem_ex11.cpp
@@ -23,4 +23,24 @@ class Quad{
 };
 
 complex<Quad> cq;
-complex<int> ci = cq; //error, тк нет приведения   Quad к int
+//complex<int> ci = cq; //error, тк нет приведения   Quad к int
+
+
+/*К сожалению, C++ допускает некоторые неразумные операции преобразования
+        встроенных типов, такие как double в int. Сопутствующие проблемы потери
+        точности можно обнаружить во время выполнения с помощью проверяемых
+преобразований в стиле implicit_cast и функции checked()*/
+
+template<class Scalar> class new_complex{
+    Scalar re, im;
+public:
+    new_complex():re(0), im(0){}
+    new_complex(const new_complex<Scalar>& c):re(checked_cast<Scalar> (c. real ())) , im (checked_cast<Scalar> (c. imag () ) ) { }
+};
+
+
+//Шаблонная функция-член не может быть виртуальной. Например:
+class Shape
+{
+    //template<class T> virtual boot_intersect {const T&) const=0; //error: virtual template
+    };
