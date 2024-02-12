1d6640dd25a521e0f41729c1d80080c379675ce1
Daniil <danast_2@mail.ru>
2024-02-12 07:22:49 +0300

    12.02.2024/07:22

---
+++ exceptions/exep_ex12.cpp
@@ -0,0 +1,35 @@
+//Исключения, не являющиеся ошибками
+
+//можно рассматривать механизм обработки исключений как еще одну управляющую структуру.
+//Например:
+
+void func(Queue <X> &q) {
+    try {
+        for (;;) {
+            X m = q.get();
+        }
+    } catch (Queue<X>::Empty) {
+        return;
+    }
+}
+
+/*В этом примере есть свое очарование, ибо это как раз тот случай, когда
+невозможно определенным образом сказать, что тут ошибка, а что нет.*/
+
+void fnd(Tree *p, const strings s) {
+    if (s == p->str) throw p; //s найдено
+    if (p->left) fnd(p->left, s);
+    if (p->right) fnd(p->right, s);
+}
+
+Tree *find(Tree *p, const string& s) {
+    try {
+        fnd(p, s);
+        }
+        catch (Tree * q) // q->str==s
+        {
+            return q;
+        }
+        return 0;
+    }
+}
