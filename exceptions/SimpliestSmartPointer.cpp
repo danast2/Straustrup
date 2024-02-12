92822d5c6941b346f41602fffec33507e6219fbe
Daniil <danast_2@mail.ru>
2024-02-12 06:00:20 +0300

    12.02.2024/06:00

--- exceptions/SimpliestSmartPointer.cpp
+++ exceptions/SimpliestSmartPointer.cpp
@@ -4,16 +4,16 @@ private:
     T* p;
 public:
     SmartPointer(T* ptr):p(ptr){}
-    //SmartPointer(T ptr):p(&ptr){}
+    SmartPointer(T ptr):p(&ptr){}
     ~SmartPointer(){delete p;}
     T& get_pointer()const{ return *(p); }
 };
 
 int main(){
     int y = 5;
-    //лучше не передавать указатели, не полученные при динамическом выделении памяти
-    //SmartPointer<int> z (&y);
-    //std::cout << z.get_pointer();
+    //лучше не передавать что-то не полученное при динамическом выделении памяти
+    //SmartPointer<int> z  =  (&y);
+    //std::cout << z.get_pointer() << std::endl;
     SmartPointer<int> x = new int(5);
     std::cout << x.get_pointer();
     return 0;
