f631d5c9628c783d9f4fe52c2adcc07a3b99f47c
Daniil <danast_2@mail.ru>
2024-02-14 07:11:10 +0300

    14.02.2024/07:11

--- namespaces_and_exceptions/nae_ex6.cpp
+++ namespaces_and_exceptions/nae_ex6.cpp
@@ -1,4 +1,5 @@
 //пример Exception
+#include <iostream>
 
 namespace Exception{
     int no_of_errors;
@@ -9,4 +10,15 @@ namespace Exception{
         const char* p;
         Syntax_error(const char* q) {p = q; no_of_errors++;}
     };
+}
+
+int main(){
+    try{
+
+    }catch (Exception::Zero_divide){
+
+    }catch (Exception::Syntax_error e){
+        std::cerr << "syntax error" << e.p << "\n";
+    }
+    //...........................
 }
